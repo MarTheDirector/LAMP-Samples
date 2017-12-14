e494f18a15471e9d3b16804b2a30e1a8f70da8f8
AdventureOfMar <Nesbitta3@winthrop.edu>
2017-12-14 18:48:36 -0500

    Push
    Hackathon project

---
+++ hackathon15-master/public_html/about.php
@@ -0,0 +1,48 @@
+﻿<?php
+	include_once "header.php";
+	?>
+
+	
+		<div class="row">
+			<div class="col-xs-12 headerBackgroundAbout">
+				<div class="row">
+					<div class="col-xs-12 icon text-right">
+						<img src="images/login.png" class="pull-right" style="margin:15px;">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 text-center">
+						<div class="col-centered">	<!-- Nav -->
+							<div class="col-xs-5 col-xs-offset-3 text-center title" style="margin-left:30%;">
+								<h1 class="drop">Next Chapter</h1>
+								<h2 class="drop">Turn the Page on Your Career</h2>
+								<h2 class="drop">About</h2>
+								<div class="row">
+									<div class="col-xs-12 describe">
+										<p>Next Chapter is a career engagement website focused on empowering individuals to turn the page to the next chapter in their life. Instead of applying to hundreds of jobs online, we allow the businesses to find you. What are you waiting for? Star the next chapter in your life now.</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-1 col-xs-pull-1">
+								<img src="images/logo.png" alt="logo" style="margin-left:30px;" class="visible-lg visible-md">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br><br><br><br><br><br><br>
+		</div>
+		
+		<div class="row footer">
+			<div class="col-xs-12">
+				<img src="images/logo.png" class="footerLogo" alt="logo">
+					<img class="pull-right" src="images/iconLinkedin.png" alt="LinkedIn logo">
+					<img class="pull-right" src="images/iconTwitter.png" alt="LinkedIn logo">
+					<img class="pull-right" src="images/iconFacebook.png" alt="LinkedIn logo">
+					<img class="pull-right" src="images/iconInsta.png" alt="LinkedIn logo">
+					<img class="pull-right" src="images/iconInsta.png" alt="LinkedIn logo">
+			</div>
+		</div>
+	</body>
+</html>
+
---
+++ hackathon15-master/public_html/header.php
@@ -0,0 +1,61 @@
+<?php
+	include_once "common.php";
+?>
+<html>
+	<head>
+		<script src="js/jquery.js" ></script>
+		<script src="js/main.js" ></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+		
+		<link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="js/bootstrap.min.js" ></script>
+		<link rel="stylesheet" href="css/main.css">
+
+	</head>
+
+	<body>
+
+		<div class="row navi">
+			<a href="index.php">
+				<div class="col-md-1 text-center mainNav visible-md visible-lg">
+					<img src="images/logo.png" width="35px">
+				</div>
+			</a>
+			<a href="schools.php">
+				<div class="col-md-2 text-center mainNav">
+					<h4>Colleges</h4>
+				</div>
+			</a>
+			<a href="majors.php">
+				<div class="col-md-3 text-center mainNav">
+					<h4>Majors</h4>
+				</div>
+			</a>
+			<a href="about.html">
+				<div class="col-md-3 text-center mainNav">
+					<h4>About</h4>
+				</div>
+			</a>
+				<div class="col-md-3 mainNav">
+						<div class="row" style="padding-top:9px;">
+							<div class="col-xs-3">
+								<span class="glyphicon glyphicon-search searchIcon"></span>
+							</div>
+
+							<div class="col-xs-9 col-xs-pull-1 searchBar">
+							<input type="text" class="search" >
+							</div>
+
+						</div>
+
+				</div>
+
+ 
+		</div>
+
